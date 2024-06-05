37724a3b957416f96a33bfc48371ee442f3b21b8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-06 01:08:48 +0200

    update style , add image movements and  game algorithims

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const allImgs = document.querySelectorAll("img");
+const header = document.querySelector(".header");
 const start = document.querySelector(".start");
 const timerDisplay = document.querySelector(".timer");
 const score = document.querySelector(".score");
 const again = document.querySelector(".again");
 const audio = document.querySelector("#audio");
 
-allImgs.forEach((image) => {
-  image.classList.add("close");
-});
-
 //! Timer
 
-let totalTime = 180; // 3 dakika, 60 saniye * 3 = 180 saniye
+let totalTime = 10; // 3 dakika, 60 saniye * 3 = 180 saniye
 
 const timer = setInterval(() => {
   const minutes = Math.floor(totalTime / 60);
@@ -45,11 +42,54 @@ function toStart() {
   audio.volume = 0.1;
 }
 
+toStart();
+
+//? Variables
+
+let points = 0;
+let activeOne = false;
+let activeTwo = false;
+
 //! Start Button
 start.addEventListener("click", () => {
   toStart();
   start.classList.add("none");
   timerDisplay.classList.remove("none");
-});
+  // ! Image Movements
+  allImgs.forEach((image) => {
+    image.addEventListener("click", () => {
+      if (!activeOne) {
+        activeOne = image;
+        activeOne.classList.remove("close");
+        activeOne.classList.add("open");
+      } else if (!activeTwo && image !== activeOne) {
+        activeTwo = image;
+        activeTwo.classList.remove("close");
+        activeTwo.classList.add("open");
 
-allImgs.forEach((image) => {});
+        //! Control
+        setTimeout(() => {
+          if (activeOne.src === activeTwo.src) {
+            points++;
+            score.textContent = points;
+            if (points === 10) {
+              header.textContent = "Herzlichen Glückwunsch!";
+            }
+          } else {
+            activeOne.classList.remove("open");
+            activeOne.classList.add("close");
+            activeTwo.classList.remove("open");
+            activeTwo.classList.add("close");
+          }
+          activeOne = false;
+          activeTwo = false;
+
+          if (totalTime <= 0 || points !== 10) {
+            again.classList.remove("none");
+            again.classList.add("block");
+          }
+        }, 1000);
+      }
+    });
+  });
+});
